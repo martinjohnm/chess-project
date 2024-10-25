eb3bba6d4ca95559c8ab3b19a1aa95b341f686ec
martinjohnm <johnmmartin74@gmail.com>
2024-10-25 20:05:35 +0530

    ws initial commit

---
+++ ws-backend/src/index.ts
@@ -0,0 +1,14 @@
+import { configDotenv } from "dotenv";
+import { WebSocketServer } from "ws";
+import { GameManager } from "./GameManager";
+
+configDotenv()
+
+const port = Number(process.env.PORT)
+const wss = new WebSocketServer({ port })
+
+const gameManager = new GameManager()
+
+wss.on("connection", function connection(ws) {
+    gameManager.addUser(ws)
+})
